8209d4940d974429bd56d5e64bba78d3c4b95c57
Zhnmsn <Zhnmsn@yandex.ru>
2024-08-18 20:23:32 +0300

    edit 3 lesson 5 module

--- js/modules/builder.js
+++ js/modules/builder.js
@@ -1,18 +1,63 @@
 
 
-const tBody = document.querySelector('tbody');
+const createRow = (id, title, category, count, units, price, sum, btn1, btn2, btn3 ) => {
+    
+    const tBody = document.querySelector('tbody');
+        
+    const tr = document.createElement('tr');
+    tBody.append(tr);
+    
+    const tdTitle = document.createElement('td');
+    tdTitle.textContent = title;
+    tdTitle.classList.add('.table__about');
 
-const renderGoods = (obj) => {
-for (const elem of obj) {
-    const row = tBody.insertRow();
-    for (const key in elem) {
-      const cell = row.insertCell();
-      const text = document.createTextNode(elem[key]);
-      cell.appendChild(text);
-    }
-} };
+    const tdCategory = document.createElement('td');
+    tdCategory.textContent = category;
+    tdCategory.classList.add('.table__about-m');
 
-export default renderGoods;   
+    const tdId = document.createElement('td');
+    tdId.textContent = id;
+    tdId.classList.add('.table__number');
+    
+    const tdCount = document.createElement('td');
+    tdCount.textContent = count;
+    tdCount.classList.add('.table__amount-m');
+
+    const tdUnits = document.createElement('td');
+    tdUnits.textContent = units;
+    tdUnits.classList.add('.table__amount');
+
+    const tdPrice = document.createElement('td');
+    tdPrice.textContent = price;
+    tdPrice.classList.add('.table__price');
+
+    const tdSum = document.createElement('td');
+    tdSum.textContent = sum;
+    tdSum.classList.add('.thead__all-price');
+
+    const tdBtn1= document.createElement('td');
+    tdBtn1.textContent = btn1;
+    tdBtn1.classList.add('.table__all-btn');
+
+    const tdBtn2 = document.createElement('td');
+    tdBtn2.textContent = btn2;
+    tdBtn2.classList.add('.table__all-btn');
+
+    const tdBtn3 = document.createElement('td');
+    tdBtn3.textContent = btn3;
+    tdBtn3.classList.add('.table__all-btn');
+    
+    tr.append(tdId, tdTitle, tdCategory, tdCount, tdUnits, tdPrice, tdSum, tdBtn1, tdBtn2, tdBtn3);
+
+    return tr;
+}
 
 
+    const renderGoods =(item, data) => {
+        const newMap = data.map(createRow);
+            item.append(...newMap); 
+            return newMap;
+        
+    };
 
+    export default renderGoods;
